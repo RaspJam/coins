33630c75897d66319b2c93066ce99b7d350d3a35
ZackeryRSmith <zackery.smith82307@gmail.com>
2025-02-01 15:39:23 -0500

    ignore labels and comments
    This would've happened anyway assuming an annotation isn't embbeded in the comment. This check is here for the off chance that does happen.

--- src/file.py
+++ src/file.py
@@ -3,6 +3,14 @@ import re
 
 SUPPORTED_LABELS = ("COMMAND", "OPTION", "INPUT", "NUMBER", "STRING")
 
+# TODO:
+#   - Potentially add support for an "or" inside of the square brackets
+#     essentially, this would generate two lines with the same text replacing
+#     the entities. E.g. say [hi || hello](STRING) would generate two lines:
+#     "say [hi](STRING)" and "say [hello](STRING)"
+#  - Potentially add support for more labels "under the hood". For example,
+#    a STRING could be a location, a person, etc. Almost acting as a kind of
+#    "sub-label". This may make using COMMANDS a bit eaiser however
 def annotation_to_data(path: str) -> list[tuple[str, dict]]:
     """
     Extracts annotations from a text file, returning usable training data.
@@ -17,6 +25,8 @@ def annotation_to_data(path: str) -> list[tuple[str, dict]]:
     data = []
     for line in annotations:
         raw_line = re.sub(pattern, r'\1', line).strip()
+        if raw_line.startswith(("//", "#")):
+            continue # ignore comments and labels
 
         entities = []
         for match in list(re.finditer(pattern, line)):
