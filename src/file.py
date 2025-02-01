8fae6e5924fd7a083e858f0341e711c39f6fc8f3
ZackeryRSmith <zackery.smith82307@gmail.com>
2025-02-01 09:27:34 -0500

    fix annotation parsing
    a given annotation doesn't appear inside the training data anymore

--- src/file.py
+++ src/file.py
@@ -7,29 +7,28 @@ def annotation_to_data(path: str) -> list[tuple[str, dict]]:
     """
     Extracts annotations from a text file, returning usable training data.
     """
-    with open(path, "r") as annotations:
-        text = annotations.readlines()
+    with open(path, "r") as f:
+        annotations = f.readlines()
 
     # matches text within square brackets or parentheses
-    pattern = re.compile(r"\[(?P<entity>.*?)\]|\((?P<label>.*?)\)")
+    #pattern = re.compile(r"\[(?P<entity>.*?)\]|\((?P<label>.*?)\)")
+    pattern = re.compile(r"\[(?P<entity>.*?)\]\((?P<label>.*?)\)")
 
     data = []
-    for line in text:
-        line = line.strip('\n')
+    for line in annotations:
+        raw_line = re.sub(pattern, r'\1', line).strip()
 
-        matches = list(re.finditer(pattern, line))
         entities = []
-
-        for entity, label in zip(matches[::2], matches[1::2]):
-            label = label.group("label")
+        for match in list(re.finditer(pattern, line)):
+            entity, label = match.group("entity", "label")
             if not is_supported_label(label):
-                print(f'Unsupported label: "{label}". Ignoring...')
-                continue
+                raise ValueError(f'Unsupported label: "{label}"!')
 
-            # NOTE: The start and end indices are offset by 1 because the pattern includes the brackets
-            entities.append((entity.start() + 1, entity.end() - 1, label))
+            start = raw_line.find(match.group("entity"))
+            end = start + len(match.group("entity"))
 
-        data.append((line, {"entities": entities}))
+            entities.append((start, end, label))
+        data.append((raw_line, {"entities": entities}))
     return data
 
 def is_supported_label(label: str) -> bool:
