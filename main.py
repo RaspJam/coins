1da369b9c0fd875f45320aaca41938a8226937f5
ZackeryRSmith <zackery.smith82307@gmail.com>
2025-02-01 09:29:32 -0500

    minor fixes & cleanups
    progress bar on # of iters, skip pipe check, remove losses debug, properly create the pipe

--- main.py
+++ main.py
@@ -12,23 +12,19 @@ NUM_OF_ITERATIONS = 100
 # TODO: Ask the user if they'd like to train a new model or use an existing one
 nlp = spacy.blank("en")
 
-nlp.add_pipe('ner', last=True)
-ner = nlp.get_pipe('ner')
+ner = nlp.create_pipe("ner")
+nlp.add_pipe("ner", last=True)
 
 for label in SUPPORTED_LABELS:
     ner.add_label(label)
 
-other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
-with nlp.disable_pipes(*other_pipes):  # only train NER
-    optimizer = nlp.begin_training()
-    for itn in range(NUM_OF_ITERATIONS):
-        random.shuffle(TRAIN_DATA)
-        losses = {}
-        for text, annotations in tqdm(TRAIN_DATA):
-            doc = nlp.make_doc(text)
-            example = Example.from_dict(doc, annotations)
-            nlp.update([example], losses=losses, drop=0.3, sgd=optimizer)
-        print(losses)
+optimizer = nlp.begin_training()
+for itn in tqdm(range(NUM_OF_ITERATIONS)):
+    random.shuffle(TRAIN_DATA)
+    for text, annotations in TRAIN_DATA:
+        doc = nlp.make_doc(text)
+        example = Example.from_dict(doc, annotations)
+        nlp.update([example], drop=0.5, sgd=optimizer)
 
 
 if OUTPUT_DIRECTORY is not None:
